03501a82b4cc4e43efc6d084ade64611373fbb80
Amrita <amritakataria515@gmail.com>
2025-04-02 15:39:26 +0530

    subtree of another tree

---
+++ 15-Binary Tree/SubtreeOfAnotherTree.java
@@ -0,0 +1,54 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class SubtreeOfAnotherTree {
+    public static boolean isIdentical(Node root, Node subRoot){
+        if(root==null && subRoot==null){
+            return true;
+        } else if(root==null || subRoot==null || root.data !=subRoot.data){
+            return false;
+        } 
+        if(!isIdentical(root.left, subRoot.left)){
+            return false;
+        }
+        if(!isIdentical(root.right, subRoot.right)){
+            return false;
+        }
+
+        return true;
+    }
+    public static boolean isSubtree(Node root, Node subRoot){
+        if(root==null){
+            return false;
+        } 
+        if(root.data == subRoot.data){
+            if(isIdentical(root, subRoot)){
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.right = new Node(3);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(6);
+        subRoot.right = new Node(5);
+        System.out.println(isSubtree(root, subRoot));
+    }
+}
