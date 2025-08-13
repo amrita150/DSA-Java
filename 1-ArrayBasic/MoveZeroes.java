45cd89323b62956af44dd71b2641ef373b970ab3
Amrita <amritakataria515@gmail.com>
2025-08-13 19:05:00 +0530

    move zeroes to end

--- 1-ArrayBasic/MoveZeroes.java
+++ 1-ArrayBasic/MoveZeroes.java
@@ -18,29 +18,20 @@ public class MoveZeroes {
     //optimized approach, using two pointer and swapping
     
     public static void moveZeroesToEnd(int arr[]){
-        int j = -1;
-        //point j to the place where element is zero
-        for(int i =0; i<arr.length;i++){
+        int i = 0;
+        int j = arr.length -1;
+        while(i<j){
             if(arr[i]==0){
-                j=i;
-                break;
-            }
-        }
-
-        //check if array contain any zeroes or not
-        if(j == -1){
-            return;
-        }
-
-        //do swapping
-        for(int i = j+1; i<arr.length ; i++){
-            if(arr[i]!=0){
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-                j++;
+                j--;
+            } else{
+                i++;
             }
         }
+
+       
 
         printArray(arr);
     }
